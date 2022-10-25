d9a1e1c9e0a1a75e0848e471eeef24bb92dfe4d6
Igor Pereira Dourado <70119242+IgorDouradoBR@users.noreply.github.com>
2022-10-24 22:06:51 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.security.*;
 import java.util.*;
@@ -16,48 +15,50 @@ import java.nio.file.Files;
  */
 public class Main {
 
-    private static MessageDigest md;
 
-    static {try {md = MessageDigest.getInstance("SHA-256");}
-    catch (NoSuchAlgorithmException e) { }}
 
     //separa o video em uma matriz de bytes, cada bloco tem 1024 bytes, o ultimo bloco pode ser menor se o tamanho do video nao for multiplo de 1024
-    private static byte[][] preencherBlocos(byte[] video) {
-        int ultimoIndex = 0;
-        int qtdBlocos = video.length % 1024 != 0 ?  (video.length / 1024 + 1) :  video.length / 1024;
-        byte[][] blocos = new byte[qtdBlocos][1024];
+    private static byte[][] init(byte[] video) {
+        int numBlocks = video.length % 1024 != 0 ?  (video.length / 1024 + 1) :  video.length / 1024;
+        int contPos = 0;
+        byte[][] blocks = new byte[numBlocks][1024];
 
-        for (int i = 0; i < qtdBlocos; i++) {
-            for (int j = 0; j < 1024 && ultimoIndex < video.length; j++) {
-                blocos[i][j] = video[ultimoIndex];
-                ultimoIndex++;
+        for (int i = 0; i < numBlocks; i++) {
+            for (int j = 0; j < 1024 && contPos < video.length; j++) {
+                blocks[i][j] = video[contPos];
+                contPos++;
             }
         }
-        if(blocos==null || md == null){
+        if(blocks==null || contPos <= -1){
             return null;
         }
-        return blocos;
+        return blocks;
     }
+
+    static MessageDigest algorithm;
+
+    static {try {algorithm = MessageDigest.getInstance("SHA-256");}
+    catch (NoSuchAlgorithmException nsae) { }}
 
         //metodo que printa o H0, parte do ultimo bloco do video, gera o hash, concatena esse hash no final do bloco anterior,
     // gera o hash, concatena.. até chegar no primeiro bloco de bytes
-    private static void buscarH0(byte[][] blocos, int length) throws IOException {
-        byte[][] blocosHash = new byte[blocos.length][];
-        byte[] ultimoBloco = Arrays.copyOfRange(blocos[blocos.length - 1], 0, length % 1024);
-        blocosHash[blocos.length - 1] = md.digest(ultimoBloco); //gera hash do ultimo bloco
+    private static void findBlock(byte[][] blocks, int length) throws IOException {
+        byte[][] blocksResume = new byte[blocks.length][];
+        byte[] ultimoBloco = Arrays.copyOfRange(blocks[blocks.length - 1], 0, length % 1024);
+        blocksResume[blocks.length - 1] = algorithm.digest(ultimoBloco); //gera hash do ultimo bloco
 
         //parte do penultimo bloco ate o primeiro, gerando hash e concatenando no bloco anterior
-        for (int i = blocos.length - 2; i >= 0; i--) {
+        for (int i = blocks.length - 2; i >= 0; i--) {
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            outputStream.write(blocos[i]);
-            outputStream.write(blocosHash[i + 1]);
-            blocosHash[i] = md.digest(outputStream.toByteArray());
+            outputStream.write(blocks[i]);
+            outputStream.write(blocksResume[i + 1]);
+            blocksResume[i] = algorithm.digest(outputStream.toByteArray());
         }
         StringBuilder sb = new StringBuilder();
 
         //transforma o hash de array de bytes para hexadecimal
-        for (int i = 0; i < blocosHash[0].length; i++) {
-            sb.append(Integer.toString((blocosHash[0][i] & 0xff) + 0x100, 16).substring(1));
+        for (int i = 0; i < blocksResume[0].length; i++) {
+            sb.append(Integer.toString((blocksResume[0][i] & 0xff) + 0x100, 16).substring(1));
 
         }
         System.out.println("Digest(in hex format):: " + sb.toString());
@@ -69,7 +70,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("\n Digite o nome do arquivo e sua extensão");
         String entrada = scanner.nextLine();      
-        buscarH0(preencherBlocos(Files.readAllBytes(new File(entrada).toPath())), Files.readAllBytes(new File(entrada).toPath()).length);
+        findBlock(init(Files.readAllBytes(new File(entrada).toPath())), Files.readAllBytes(new File(entrada).toPath()).length);
     }
 
 
@@ -78,7 +79,7 @@ public class Main {
 
     /*metodo para gerar hash de um array de bytes utilizando a biblioteca MessageDigest
     private static byte[] gerarHash(byte[] bloco) {
-        return md.digest(bloco);
+        return algorithm.digest(bloco);
     }
     */
 
